eccddf4db885493d6d6780a2c2076493930da7cd
LeopoldHock <ameisenfutter@outlook.com>
2022-05-18 15:15:17 +0200

    made most variables local

--- FullUIToggle.lua
+++ FullUIToggle.lua
@@ -1,50 +1,51 @@
+FullUIToggle = {};
+
 -- Debug mode --
-DebugMode = false;
+local debugMode = false;
 
 -- Global runtime variables --
-UiIsHidden = false;
-OriginalUnitNameOwnState = nil;
-OriginalUnitNameNPCState = nil;
-OriginalUnitNamePlayerState = nil;
+local uiIsHidden = false;
+local originalUnitNameOwnState = nil;
+local originalUnitNameNPCState = nil;
+local originalUnitNamePlayerState = nil;
 
-
-function Toggle()
-    if (UiIsHidden == false) then
+function FullUIToggle.Toggle()
+    if (uiIsHidden == false) then
         Debug("Hiding UI.");
         UIParent:Hide();
         RememberUnitnamestates();
-        UiIsHidden = true;
+        uiIsHidden = true;
     else
         Debug("Showing UI.");
         UIParent:Show();
         RestoreUnitNameStates();
-        UiIsHidden = false;
+        uiIsHidden = false;
     end
 end
 
 function RememberUnitnamestates()
-    OriginalUnitNameOwnState = GetCVar("UnitNameOwn");
-    OriginalUnitNameNPCState = GetCVar("UnitNameNPC");
-    OriginalUnitNamePlayerState = GetCVar("UnitNamePlayer");
+    originalUnitNameOwnState = GetCVar("UnitNameOwn");
+    originalUnitNameNPCState = GetCVar("UnitNameNPC");
+    originalUnitNamePlayerState = GetCVar("UnitNamePlayer");
     SetCVar("UnitNameOwn", 0);
     SetCVar("UnitNameNPC", 0);
     SetCVar("UnitNamePlayer", 0);
-    Debug("Saved state of UnitNameOwn (" .. OriginalUnitNameOwnState .. ") and set it to 0.");
-    Debug("Saved state of UnitNameNPC (" .. OriginalUnitNameNPCState .. ") and set it to 0.");
-    Debug("Saved state of UnitNamePlayer (" .. OriginalUnitNamePlayerState .. ") and set it to 0.");
+    Debug("Saved state of UnitNameOwn (" .. originalUnitNameOwnState .. ") and set it to 0.");
+    Debug("Saved state of UnitNameNPC (" .. originalUnitNameNPCState .. ") and set it to 0.");
+    Debug("Saved state of UnitNamePlayer (" .. originalUnitNamePlayerState .. ") and set it to 0.");
 end
 
 function RestoreUnitNameStates()
-    SetCVar("UnitNameOwn", OriginalUnitNameOwnState);
-    SetCVar("UnitNameNPC", OriginalUnitNameOwnState);
-    SetCVar("UnitNamePlayer", OriginalUnitNameOwnState);
-    Debug("Restored state of UnitNameOwn (" .. OriginalUnitNameOwnState .. ").");
-    Debug("Restored state of UnitNameNPC (" .. OriginalUnitNameNPCState .. ").");
-    Debug("Restored state of UnitNamePlayer (" .. OriginalUnitNamePlayerState .. ").");
+    SetCVar("UnitNameOwn", originalUnitNameOwnState);
+    SetCVar("UnitNameNPC", originalUnitNameOwnState);
+    SetCVar("UnitNamePlayer", originalUnitNameOwnState);
+    Debug("Restored state of UnitNameOwn (" .. originalUnitNameOwnState .. ").");
+    Debug("Restored state of UnitNameNPC (" .. originalUnitNameNPCState .. ").");
+    Debug("Restored state of UnitNamePlayer (" .. originalUnitNamePlayerState .. ").");
 end
 
 function Debug(text)
-    if (DebugMode) then
+    if (debugMode) then
         DEFAULT_CHAT_FRAME:AddMessage("[FUIT] " .. text, 1, 1, 1);
     end
 end
